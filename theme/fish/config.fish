a66cfd7dc57e6353d2c36fc73a95a266a9222fb8
Mizosu97 <earthpig97@gmail.com>
2024-09-01 01:07:34 -0400

    fish

--- theme/fish/config.fish
+++ theme/fish/config.fish
@@ -3,7 +3,7 @@ if status is-interactive
 	end
 
 	function fish_prompt
-		echo -e "\x1b[38;2;186;194;222m┌──(\x1b[38;2;137;180;250m$(whoami)@$hostname\x1b[38;2;186;194;222m)─[\x1b[38;2;245;194;231m$PWD\x1b[38;2;186;194;222m]"
+		echo -e "\x1b[38;2;92;95;119m┌──(\x1b[38;2;30;102;245m$(whoami)@$hostname\x1b[38;2;92;95;119m)─[\x1b[38;2;234;118;203m$PWD\x1b[38;2;92;95;119m]"
 		echo -e "└─\\ "
 	end
 end
